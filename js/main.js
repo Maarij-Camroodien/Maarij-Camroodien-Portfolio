ed3262ddfca75914746ea8dc246990cc5f652b04
”Maarij-Camroodien” <maarijcamroodien18@gmail.com>
2024-03-08 16:20:54 +0200

    Second Commit

--- js/main.js
+++ js/main.js
@@ -17,3 +17,46 @@ window.addEventListener('scroll', function() {
     header.style.backgroundColor = 'transparent'; // Revert to transparent background when the scroll position is above the '.container' element
   }
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.querySelector('.menu-btn');
+    const navbar = document.querySelector('.navbar');
+
+    menuBtn.addEventListener('click', function() {
+        navbar.classList.toggle('active');
+    });
+});
+
+
+//slider 
+
+// $(document).ready(function(){
+//   $('.arrow').on('click', function(){
+//       var currentImg = $('.active');
+//       var nextImg = currentImg.next();
+
+//       if(nextImg.length){
+//           currentImg.removeClass('active').css('z-index', -10);
+//           nextImg.addClass('active').css('z-index', 10);
+//       } else {
+//           currentImg.removeClass('active').css('z-index', -10);
+//           $('.slider-images .slides img:first').addClass('active').css('z-index', 10);
+//       }
+//   });
+// });
+
+// $(document).ready(function(){
+//   $('.arrow').on('click', function(){
+//       var currentdot = $('.active1');
+//       var nextdot = currentdot.next();
+
+//       if(nextdot.length){
+//           currentdot.removeClass('active1').css('opacity', 1);
+//           nextdot.addClass('active1').css('opacity', 0.5);
+//       } else {
+//           currentdot.removeClass('active1').css('opacity', 1);
+//           $('.slider-dots img:first').addClass('active1').css('opacity', 0.5);
+//       }
+//   });
+// });
